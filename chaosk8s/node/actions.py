d2a9b2dedd1d4d5e3aeeb2e8533ed1617cb19634
Martin Reus <martin.reus@form3.tech>
2020-06-26 15:13:16 +0100

    Fix lint error

--- chaosk8s/node/actions.py
+++ chaosk8s/node/actions.py
@@ -282,7 +282,8 @@ def drain_nodes(name: str = None, label_selector: str = None,
                              "node '{}'".format(name, node_name))
                 continue
 
-            if volumes and any(filter(lambda v: v.empty_dir is not None, volumes)):
+            if volumes and any(
+                    filter(lambda v: v.empty_dir is not None, volumes)):
                 logger.debug(
                     "Pod '{}' on node '{}' has a volume made "
                     "of a local storage".format(name, node_name))
